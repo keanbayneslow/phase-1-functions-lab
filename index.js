0b9aa6a1a5ab76c66183c1fe9eb2662dcc99d537
Kean Bayneslow <kean.bayneslow@gmail.com>
2023-08-15 14:14:45 +0800

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+
+function distanceFromHqInBlocks(pickupLocation) {
+    return Math.abs(pickupLocation - 42)
+}
+
+function distanceFromHqInFeet(pickupLocation) {
+    return distanceFromHqInBlocks(pickupLocation) * 264
+}
+
+function distanceTravelledInFeet(pickupLocation, endLocation) {
+    return Math.abs(endLocation - pickupLocation) * 264
+}
+
+function calculatesFarePrice(pickupLocation, endLocation) {
+    const distance = distanceTravelledInFeet(pickupLocation, endLocation);
+
+    if (distance <= 400) {
+        return 0;
+    } else if (distance <= 2000) {
+        return 0.02 * (distance - 400);
+    } else if (distance <= 2500) {
+        return 25;
+    } else {
+        return 'cannot travel that far';
+    }
+}
+
+let pickupLocation = 50;
